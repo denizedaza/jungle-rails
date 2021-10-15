cbe12ecccc34dfd8869deed02d5a9737e5df94f1
denizedaza <dazadenize@gmail.com>
2021-10-15 17:58:17 -0400

    create routes for user login sessions

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,14 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
