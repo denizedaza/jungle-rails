75d8c07cc18b2df4052ba3da57567d4ea74bf4f3
denizedaza <dazadenize@gmail.com>
2021-10-20 13:53:14 -0400

    add rspec test for add to cart

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+    # SETUP
+    before :each do
+      @category = Category.create! name: 'Apparel'
+  
+      10.times do |n|
+        @category.products.create!(
+          name:  Faker::Hipster.sentence(3),
+          description: Faker::Hipster.paragraph(4),
+          image: open_asset('apparel1.jpg'),
+          quantity: 10,
+          price: 64.99
+        )
+      end
+    end
+  
+    scenario "My Cart updates when product is added" do
+      # ACT
+      visit root_path
+  
+      # DEBUG / VERIFY
+      save_screenshot
+     
+     
+      expect(page).to have_content("My Cart (0)")
+
+      find_button("Add", match: :first).click
+      sleep 3
+
+      expect(page).to have_content("My Cart (1)")
+    end
+end
